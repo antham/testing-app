1dbefa213197eda2d60e2e36b9f6c44755d89faf
Anthony HAMON <anthony@koyeb.com>
2022-05-31 20:00:17 +0200

    Add exclamation marks

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type Response struct {
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		b, err := json.Marshal(Response{Message: "Hello World !"})
+		b, err := json.Marshal(Response{Message: "Hello World !!!!!"})
 		if err != nil {
 			log.Fatal(err)
 		}
